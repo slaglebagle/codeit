ecd1b10e53288632b44265435cbbcddf94973424
slaglebagle <jacobpslagle@gmail.com>
2015-10-05 13:21:20 -0400

    Create Intro-sql-lesson-1-A.sql

---
+++ Intro-sql-lesson-1-A.sql
@@ -0,0 +1,12 @@
+/* Welcome to Code It "Learn Coding" *\
+
+/* In this section we are going to take a look at, what is a database? Why do we do we uses databases? and how database function.
+Generally you don't need to know a lot about coding, but you will need to pay close attention to this course because when we
+start learning about SQL statements and syntax it can be very tedious at time and I want to make sure you have a general
+understanding of databases. So just relax and slowly read the lessons and feel free to make necessary post on the forums if you
+get stuck. Generally I will reply back within a few hours after you post something so please don't expect me to reply right away
+. My contact information will also be listed, but it would be better if you posted on the forum because I can easily understand
+what you are doing and help you resolve your problem a lot faster. Enjoy this course! :) */
+
+
+
